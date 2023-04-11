e8060206230bbc87274fa7a7af682e24ce32c972
pwwang <pwwang@pwwang.com>
2023-04-11 11:49:18 -0700

    🔖 0.3.0

--- simplug.py
+++ simplug.py
@@ -18,7 +18,7 @@ from typing import (
 
 from diot import OrderedDiot
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 SimplugImpl = namedtuple("SimplugImpl", ["impl", "has_self"])
 SimplugImpl.__doc__ = """A namedtuple wrapper for hook implementation.
